759c94e55714b5b6dcc66ab71912333601ed6d6a
Catlerc <mcr1123@yandex.ru>
2024-01-27 20:39:29 +0500

    lvl2

--- Assets/Scripts/Magnit.cs
+++ Assets/Scripts/Magnit.cs
@@ -6,15 +6,25 @@ public class Magnit : MonoBehaviour
     public Crane crane;
     public Gosling gosling;
 
+    public Rigidbody[] toUnkinematic;
+    public GameObject sysBefore;
+    public GameObject sysAfter;
+
     private void OnCollisionEnter(Collision other)
     {
-
         if (other.rigidbody != null && other.rigidbody.CompareTag("budka"))
         {
             crane.isReady = true;
             other.rigidbody.transform.SetParent(transform);
             gosling.forward = true;
             Destroy(this);
+            foreach (var rb in toUnkinematic)
+            {
+                rb.isKinematic = false;
+            }
+
+            sysBefore.SetActive(false);
+            sysAfter.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    public Transform barier;
+    public GameObject stone;
+    public GameObject staticStone;
+    public PlayerBarier playerBarier;
+
+    private void Update()
+    {
+        if (transform.position.z < barier.position.z)
+        {
+            stone.SetActive(false);
+            staticStone.SetActive(true);
+            Destroy(this);
+            Destroy(playerBarier);
+        }
+    }
+}
